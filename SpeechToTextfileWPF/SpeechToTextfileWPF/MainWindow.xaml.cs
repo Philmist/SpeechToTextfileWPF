48f49eace3e4edaaa2ab28faca4d952534b50591
philmist <philmist.j@gmail.com>
2020-04-27 16:11:08 +0900

    recognizerを処理するためのイベントの一部を実装した。

--- SpeechToTextfileWPF/SpeechToTextfileWPF/MainWindow.xaml.cs
+++ SpeechToTextfileWPF/SpeechToTextfileWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech;
 
 namespace SpeechToTextfileWPF
 {
@@ -20,9 +23,91 @@ namespace SpeechToTextfileWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string subscriptionKey;
+        private string serviceRegion;
+
+        private bool isListening = false;
+        private ConcurrentQueue<string> textQueue;
+
+        private SpeechRecognizer recognizer = null;
+        private AudioConfig audioConfig = null;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 現在のコントロールを有効化したり無効化したりする
+        /// </summary>
+        /// <param name="state">有効化するならtrue</param>
+        /// <returns></returns>
+        private async Task changeControls(bool state)
+        {
+            switch (state)
+            {
+                case true:
+                    Dispatcher.Invoke(() =>
+                    {
+                        RefreshSecondSlider.IsEnabled = true;
+                        AzureSubscriptionPanel.Visibility = Visibility.Visible;
+                        FileSelectButton.IsEnabled = true;
+                        RecognizeButton.Content = "Recognize";
+                    });
+                    break;
+                default:
+                    await Task.Run(() =>
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            RefreshSecondSlider.IsEnabled = false;
+                            AzureSubscriptionPanel.Visibility = Visibility.Hidden;
+                            FileSelectButton.IsEnabled = false;
+                            RecognizeButton.Content = "Stop";
+                        });
+                    });
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 認識用のボタンを有効化したり無効化したりする
+        /// </summary>
+        /// <param name="state">有効化するならtrue、無効化するならfalse</param>
+        /// <returns></returns>
+        private async Task changeStateRecognizeButton(bool state)
+        {
+            await Task.Run(() =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    RecognizeButton.IsEnabled = state;
+                });
+            });
+
+        }
+
+        private async void UpdateRecognizedText(object sender, SpeechRecognitionEventArgs eventArgs)
+        {
+            if (eventArgs.Result.Reason == ResultReason.RecognizedSpeech)
+            {
+                await Task.Run(() => {
+                    Dispatcher.Invoke(() => { RecognizedTextBlock.Text = eventArgs.Result.Text; });
+                });
+            }
+        }
+
+        private async void RecognizeCanceled(object sender, SpeechRecognitionCanceledEventArgs eventArgs)
+        {
+            await changeControls(true);
+            await Task.Run(() =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    RecognizedTextBlock.Text = eventArgs.Reason.ToString();
+                });
+            });
+            isListening = false;
         }
     }
 }
